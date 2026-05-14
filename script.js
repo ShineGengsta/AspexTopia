9aa8df6a8abbf17a080789022e1a34d637c928ac
ShineGengsta <shinegengsta@gmail.com>
2026-05-14 13:36:08 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+// Fungsi Fetch API Server
+async function updateStats() {
+    try {
+        const response = await fetch('https://api.gtps.cloud/g-api/20034/status');
+        const data = await response.json();
+        
+        const statusText = document.getElementById('status-text');
+        const statusDot = document.getElementById('status-dot');
+        const playerCount = document.getElementById('player-online');
+
+        // Logika Status
+        if(data.status === "online") {
+            statusText.innerText = "Online";
+            statusDot.className = "dot online";
+        } else if (data.status === "maintenance") {
+            statusText.innerText = "Maintenance";
+            statusDot.className = "dot maintance";
+        } else {
+            statusText.innerText = "Offline";
+            statusDot.className = "dot offline";
+        }
+
+        // Ambil data player dari API Main
+        const mainRes = await fetch('https://api.gtps.cloud/api/server/public/2934');
+        const mainData = await mainRes.json();
+        playerCount.innerText = `${mainData.onlineCount} / ${mainData.maxPlayers || '1000'}`;
+
+    } catch (error) {
+        console.log("Error fetching API", error);
+    }
+}
+
+// Fungsi Copy
+function copyHost(text) {
+    navigator.clipboard.writeText(text);
+    alert("Host Copied!");
+}
+
+// Jalankan saat load
+window.onload = () => {
+    updateStats();
+    // Auto play music (beberapa browser butuh interaksi user dulu)
+    document.body.onclick = () => {
+        document.getElementById('bgMusic').play();
+    };
+};
